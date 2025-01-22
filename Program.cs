5b63569d215ea6a1334886d71927eca6f7eb3b55
EzeIfeanyi <134397313+EzeIfeanyi@users.noreply.github.com>
2025-01-22 15:38:17 +0100

    Task Completed

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+using TotalScoreChecker;
+
+int[] testCaseOne = {1, 2, 3, 4, 5};
+int[] testCaseTwo = { 15, 25, 35 };
+int[] testCaseThree = { 8, 8 };
+
+int TestCaseOneScore = ScoreEngine.CalculateScore(testCaseOne);
+int TestCaseTwoScore = ScoreEngine.CalculateScore(testCaseTwo);
+int TestCaseThreeScore = ScoreEngine.CalculateScore(testCaseThree);
+
+Console.WriteLine($"The Score for {string.Join(", ", testCaseOne)} is: {TestCaseOneScore}");
+Console.WriteLine($"The Score for {string.Join(", ", testCaseTwo)} is: {TestCaseTwoScore}");
+Console.WriteLine($"The Score for {string.Join(", ", testCaseThree)} is: {TestCaseThreeScore}");
---
+++ ScoreEngine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalScoreChecker;
+
+public static class ScoreEngine
+{
+    public static int CalculateScore(int[] numbers)
+    {
+        if (numbers.Length == 0) return 0;
+
+        int totalScore = 0;
+
+        foreach (int number in numbers)
+        {
+            if (number == 8) totalScore += 5;
+
+            if (number % 2 == 0)
+            {
+                totalScore += 1;
+            }
+            else
+            {
+                totalScore += 3;
+            }
+        }
+
+        return totalScore;
+
+    }
+}
